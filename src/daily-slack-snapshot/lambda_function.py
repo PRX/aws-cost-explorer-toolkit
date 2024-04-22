423302af4d627b4f478573c2d53f3bb8961195f6
Chris Kalafarski <chris@farski.com>
2024-04-22 09:49:47 -0400

    Replace SNS with EventBridge

--- src/daily-slack-snapshot/lambda_function.py
+++ src/daily-slack-snapshot/lambda_function.py
@@ -4,13 +4,8 @@ import json
 import re
 from datetime import datetime, timedelta
 
-# Set the SNS client endpoint region based on the region of the destination topic
-sns_endpoint_region = re.search(
-    r"arn:aws:sns:([a-z0-9-]+)", os.environ["SLACK_MESSAGE_RELAY_SNS_TOPIC_ARN"]
-).group(1)
-
-sns = boto3.client("sns", region_name=sns_endpoint_region)
 ce = boto3.client("ce")
+eventbridge = boto3.client("events")
 
 
 def daily_cost_chart_url(start, end):
@@ -203,118 +198,127 @@ def lambda_handler(event, context):
     # End date is exclusive
     date_end = datetime.today() - timedelta(days=0)
 
-    sns.publish(
-        TopicArn=os.environ["SLACK_MESSAGE_RELAY_SNS_TOPIC_ARN"],
-        Message=json.dumps(
+    eventbridge.put_events(
+        Entries=[
             {
-                "channel": "G9MGS7W8N",  # ops-billing
-                "username": "AWS Cost Explorer",
-                "icon_emoji": ":ops-costexplorer:",
-                "blocks": [
+                "Source": "org.prx.cost-explorer-toolkit",
+                "DetailType": "Slack Message Relay Message Payload",
+                "Detail": json.dumps(
                     {
-                        "type": "image",
-                        "title": {
-                            "type": "plain_text",
-                            "text": "EC2 Savings Plan Utilization",
-                            "emoji": True,
-                        },
-                        "image_url": daily_savings_plan_utilization_chart_url(
-                            "Amazon Elastic Compute Cloud - Compute",
-                            date_start,
-                            date_end,
-                        ),
-                        "alt_text": "EC2 savings plans utilization stacked bar chart",
-                    },
-                    {
-                        "type": "image",
-                        "title": {
-                            "type": "plain_text",
-                            "text": "EC2 Savings Plan Coverage",
-                            "emoji": True,
-                        },
-                        "image_url": daily_savings_plan_coverage_chart_url(
-                            "Amazon Elastic Compute Cloud - Compute",
-                            date_start,
-                            date_end,
-                        ),
-                        "alt_text": "EC2 savings plans coverage stacked bar chart",
-                    },
-                    {"type": "divider"},
-                    {
-                        "type": "image",
-                        "title": {
-                            "type": "plain_text",
-                            "text": "RDS Reservations Utilization",
-                            "emoji": True,
-                        },
-                        "image_url": daily_reservation_utilization_chart_url(
-                            "Amazon Relational Database Service", date_start, date_end
-                        ),
-                        "alt_text": "RDS Reservations Utilization",
-                    },
-                    {
-                        "type": "image",
-                        "title": {
-                            "type": "plain_text",
-                            "text": "RDS Reservations Coverage",
-                            "emoji": True,
-                        },
-                        "image_url": daily_reservation_coverage_chart_url(
-                            "Amazon Relational Database Service", date_start, date_end
-                        ),
-                        "alt_text": "RDS Reservations Utilization",
-                    },
-                    {"type": "divider"},
-                    {
-                        "type": "image",
-                        "title": {
-                            "type": "plain_text",
-                            "text": "ElastiCache Reservations Utilization",
-                            "emoji": True,
-                        },
-                        "image_url": daily_reservation_utilization_chart_url(
-                            "Amazon ElastiCache", date_start, date_end
-                        ),
-                        "alt_text": "ElastiCache Reservations Utilization",
-                    },
-                    {
-                        "type": "image",
-                        "title": {
-                            "type": "plain_text",
-                            "text": "ElastiCache Reservations Coverage",
-                            "emoji": True,
-                        },
-                        "image_url": daily_reservation_coverage_chart_url(
-                            "Amazon ElastiCache", date_start, date_end
-                        ),
-                        "alt_text": "ElastiCache Reservations Utilization",
-                    },
-                    {"type": "divider"},
-                    {
-                        "type": "image",
-                        "title": {
-                            "type": "plain_text",
-                            "text": "Total Daily Costs (Unblended)",
-                            "emoji": True,
-                        },
-                        "image_url": daily_cost_chart_url(
-                            datetime.today() - timedelta(days=21), date_end
-                        ),
-                        "alt_text": "Daily unblended costs stacked bar chart",
-                    },
-                    {
-                        "type": "image",
-                        "title": {
-                            "type": "plain_text",
-                            "text": "Daily Usage Costs (Unblended)",
-                            "emoji": True,
-                        },
-                        "image_url": daily_usage_cost_chart_url(
-                            datetime.today() - timedelta(days=28), date_end
-                        ),
-                        "alt_text": "Daily usage costs stacked bar chart",
-                    },
-                ],
+                        "channel": "G9MGS7W8N",  # ops-billing
+                        "username": "AWS Cost Explorer",
+                        "icon_emoji": ":ops-costexplorer:",
+                        "blocks": [
+                            {
+                                "type": "image",
+                                "title": {
+                                    "type": "plain_text",
+                                    "text": "EC2 Savings Plan Utilization",
+                                    "emoji": True,
+                                },
+                                "image_url": daily_savings_plan_utilization_chart_url(
+                                    "Amazon Elastic Compute Cloud - Compute",
+                                    date_start,
+                                    date_end,
+                                ),
+                                "alt_text": "EC2 savings plans utilization stacked bar chart",
+                            },
+                            {
+                                "type": "image",
+                                "title": {
+                                    "type": "plain_text",
+                                    "text": "EC2 Savings Plan Coverage",
+                                    "emoji": True,
+                                },
+                                "image_url": daily_savings_plan_coverage_chart_url(
+                                    "Amazon Elastic Compute Cloud - Compute",
+                                    date_start,
+                                    date_end,
+                                ),
+                                "alt_text": "EC2 savings plans coverage stacked bar chart",
+                            },
+                            {"type": "divider"},
+                            {
+                                "type": "image",
+                                "title": {
+                                    "type": "plain_text",
+                                    "text": "RDS Reservations Utilization",
+                                    "emoji": True,
+                                },
+                                "image_url": daily_reservation_utilization_chart_url(
+                                    "Amazon Relational Database Service",
+                                    date_start,
+                                    date_end,
+                                ),
+                                "alt_text": "RDS Reservations Utilization",
+                            },
+                            {
+                                "type": "image",
+                                "title": {
+                                    "type": "plain_text",
+                                    "text": "RDS Reservations Coverage",
+                                    "emoji": True,
+                                },
+                                "image_url": daily_reservation_coverage_chart_url(
+                                    "Amazon Relational Database Service",
+                                    date_start,
+                                    date_end,
+                                ),
+                                "alt_text": "RDS Reservations Utilization",
+                            },
+                            {"type": "divider"},
+                            {
+                                "type": "image",
+                                "title": {
+                                    "type": "plain_text",
+                                    "text": "ElastiCache Reservations Utilization",
+                                    "emoji": True,
+                                },
+                                "image_url": daily_reservation_utilization_chart_url(
+                                    "Amazon ElastiCache", date_start, date_end
+                                ),
+                                "alt_text": "ElastiCache Reservations Utilization",
+                            },
+                            {
+                                "type": "image",
+                                "title": {
+                                    "type": "plain_text",
+                                    "text": "ElastiCache Reservations Coverage",
+                                    "emoji": True,
+                                },
+                                "image_url": daily_reservation_coverage_chart_url(
+                                    "Amazon ElastiCache", date_start, date_end
+                                ),
+                                "alt_text": "ElastiCache Reservations Utilization",
+                            },
+                            {"type": "divider"},
+                            {
+                                "type": "image",
+                                "title": {
+                                    "type": "plain_text",
+                                    "text": "Total Daily Costs (Unblended)",
+                                    "emoji": True,
+                                },
+                                "image_url": daily_cost_chart_url(
+                                    datetime.today() - timedelta(days=21), date_end
+                                ),
+                                "alt_text": "Daily unblended costs stacked bar chart",
+                            },
+                            {
+                                "type": "image",
+                                "title": {
+                                    "type": "plain_text",
+                                    "text": "Daily Usage Costs (Unblended)",
+                                    "emoji": True,
+                                },
+                                "image_url": daily_usage_cost_chart_url(
+                                    datetime.today() - timedelta(days=28), date_end
+                                ),
+                                "alt_text": "Daily usage costs stacked bar chart",
+                            },
+                        ],
+                    }
+                ),
             }
-        ),
+        ]
     )
